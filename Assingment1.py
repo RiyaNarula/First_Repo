e3db35686068fcf8ca8e67ca495530e99df4d37b
RiyaNarula <41532740+RiyaNarula@users.noreply.github.com>
2018-08-16 23:53:43 +0530

    Create Assingment1.py

---
+++ Assingment1.py
@@ -0,0 +1,40 @@
+# Q-1: Print anything you want on screen
+
+print("Attempting The First Assignment")
+
+# Q-2: Join 2 strings using '+'
+
+str1="Acad"
+str2="View"
+print(str1+str2)
+
+# Q-3: Take the input of 3 variables x,y and z.Print their values on screen
+
+x=int(input("Enter an integer "))
+y=float(input("Enter a float value "))
+z=input("Enter a string ")
+print('x = ',x)
+print('y = ',y)
+print('z = ',z)
+
+# Q-4: Print "Let's get started" on screen
+
+print('''"Let's get started"''')
+
+# Q-5:Print the following values using placeholders.
+'''
+s="Acadview"
+course="Python"
+fees=5000
+'''
+s="Acadview"
+course="Python"
+fees=5000
+print('s: %s course: %s fees:  %d' % (s,course,fees))
+ 
+# Q-6:Let's do some intersting exercise:
+
+name="Tony stark"
+salary=1000000
+'''print('%s"%d')%(name,salary)'''
+print('%s ,%d'%(name,salary))
